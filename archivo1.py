14a89129ba1ba9d609b47ce5a286ccb172fba0b3
Sara V <rohakinlo301@gmail.com>
2019-04-19 22:24:04 -0500

    Sara V
    Eliminada la funcion resta

--- archivo1.py
+++ archivo1.py
@@ -3,8 +3,4 @@
 def suma(n, m):
     return "La suma de " + str(n) + " " + str(m) + " es: " + str(n+m)
 
-def resta(n, m):
-    return "La resta de " + str(n) + " " + str(m) + " es: " + str(n-m)
-
 suma(1, 2)
-resta(1, 2)
